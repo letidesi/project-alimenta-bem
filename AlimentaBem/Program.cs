02fec574f87bcc30494745f01d028bc4051e8566
Letícia Desiderio <leticia.desiderio@bnpsolucoes.com.br>
2024-06-16 11:12:15 -0300

    feat(project-alimenta-bem): adding the initial project structure

---
+++ AlimentaBem/Program.cs
@@ -0,0 +1,24 @@
+public partial class Program
+{
+
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+        app.Run();
+    }
+
+}
