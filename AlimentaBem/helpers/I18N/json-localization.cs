7879fca3b2eb4fc73b5c31b76ed416412997f2bb
Letícia Desiderio <leticia.desiderio@bnpsolucoes.com.br>
2024-06-16 23:44:40 -0300

    feat(localizer): add localizer

---
+++ AlimentaBem/helpers/I18N/json-localization.cs
@@ -0,0 +1,14 @@
+namespace alimenta.bem.helpers;
+
+internal class JsonLocalization
+{
+    public string Key { get; set; }
+    public Dictionary<string, string> LocalizedValues { get; set; }
+}
+
+public class I18NException : Exception
+{
+    public I18NException(string message) : base(message) {}
+    public I18NException(string message, Exception innerException) : base(message, innerException) {}
+    public I18NException() {}
+}
