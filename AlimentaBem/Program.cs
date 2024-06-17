71ba4ff6b482b3af375579b97a7593011ccf028a
Letícia Desiderio <leticia.desiderio@bnpsolucoes.com.br>
2024-06-17 01:55:21 -0300

    refactor(program): add news dependency and authorization

--- AlimentaBem/Program.cs
+++ AlimentaBem/Program.cs
@@ -1,22 +1,35 @@
 global using FastEndpoints;
 global using FluentValidation;
 global using Microsoft.EntityFrameworkCore;
-
+using alimenta.bem.helpers;
+using FastEndpoints.Security;
 using alimenta.bem.db.context;
+using System.Text.Json.Serialization;
 
 public partial class Program
 {
-
     public static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
 
         builder.Services.AddDbContext<AlimentaBemContext>(options =>
-        options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+        options.UseSqlServer(builder.Configuration.GetConnectionString("sqlConnection")));
 
-
+        builder.Services.AddAuthorization();
         builder.Services.AddEndpointsApiExplorer();
+
+        DependencyInjectionConfig.Register_Services(builder.Services);
+        CultureInfoConfig.Configure(builder.Services);
+
+        builder.Services.AddControllers();
+        builder.Services.AddFastEndpoints();
+
         builder.Services.AddSwaggerGen();
+
+        //Verify enviroment name Console.WriteLine($"{builder.Environment.EnvironmentName}");
+        var JWT_SECRET = builder.Configuration.GetSection("JWT_SECRET").Value!;
+
+        builder.Services.AddAuthenticationJwtBearer(S => S.SigningKey = JWT_SECRET);
 
         var app = builder.Build();
 
@@ -28,6 +41,13 @@ public partial class Program
         }
 
         app.UseHttpsRedirection();
+        app.UseAuthentication();
+        app.UseRouting();
+        app.UseAuthorization();
+        app.UseFastEndpoints(c => c.Serializer.Options.ReferenceHandler = ReferenceHandler.IgnoreCycles);
+        app.MapControllers();
+        app.UseOpenApi();
+
         app.Run();
     }
 }
