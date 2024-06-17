0ab69299a72bea06e90cffc9d7374fc01363a3a6
Letícia Desiderio <leticia.desiderio@bnpsolucoes.com.br>
2024-06-16 23:46:55 -0300

    feat(context): add context of project

---
+++ AlimentaBem/app-db-context/alimenta-bem-context.cs
@@ -0,0 +1,41 @@
+using System.Linq.Expressions;
+using alimenta.bem.entity.metadata;
+using alimenta.bem.user.repository;
+
+namespace alimenta.bem.db.context;
+
+public class AlimentaBemContext : DbContext
+{
+
+    public AlimentaBemContext(DbContextOptions options) : base(options) { }
+
+    #region 
+    public DbSet<User> Users { get; set; }
+
+    // public DbSet<Beneficiary> Beneficiaries { get; set; }
+
+    #endregion
+    protected override void OnModelCreating(ModelBuilder mb)
+    {
+        ConfigureSoftDelete(ref mb);
+
+        base.OnModelCreating(mb);
+    }
+    private void ConfigureSoftDelete(ref ModelBuilder mb)
+    {
+        foreach (var entityType in mb.Model.GetEntityTypes())
+        {
+            if (typeof(BaseEntity).IsAssignableFrom(entityType.ClrType))
+            {
+                var parameter = Expression.Parameter(entityType.ClrType);
+
+                var body = Expression.Equal(
+                    Expression.Property(parameter, nameof(BaseEntity.DeletedAt)),
+                    Expression.Constant(null, typeof(DateTime?))
+                );
+
+                mb.Entity(entityType.ClrType).HasQueryFilter(Expression.Lambda(body, parameter));
+            }
+        }
+    }
+}
