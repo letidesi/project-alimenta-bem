f1f4c9cf80e00f993acc83c99fa00d8dc6a121f4
Letícia Desiderio <leticia.desiderio@bnpsolucoes.com.br>
2024-06-20 09:37:14 -0300

    feat(dependecy-injection): add crypto provider

--- AlimentaBem/helpers/dependency-injection-config.cs
+++ AlimentaBem/helpers/dependency-injection-config.cs
@@ -1,9 +1,11 @@
 using alimenta.bem.helpers;
+using alimenta.bem.providers;
 
 public static class DependencyInjectionConfig
 {
     public static void Register_Services(IServiceCollection services)
     {
         services.AddSingleton<Localizer>();
+        services.AddSingleton<ICryptoProvider, CryptoService>();
     }
 }
