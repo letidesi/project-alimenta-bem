b31adae2437d3a296406ce54b3094893d5ecb8b0
Letícia Desiderio <leticia.desiderio@bnpsolucoes.com.br>
2024-06-22 10:37:29 -0300

    feat(helpers): add daly only convert and utility for string

---
+++ AlimentaBem/helpers/string-field-utilities.cs
@@ -0,0 +1,34 @@
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace alimenta.bem.helpers;
+
+public class StringUtility
+{
+    private static readonly string ENCODING = "iso-8859-7"; // GREEK Encoding
+
+    public static string? RemoveSpecialCharacter(string value)
+    {
+        if (value is null || string.IsNullOrEmpty(value?.Trim())) return null;
+
+        var sanitizedValue = Regex.Replace(value, "[^0-9a-zA-Z]+", "");
+        return sanitizedValue;
+    }
+
+    public static string? ToQueryParam(string? value)
+    {
+        if (value is null || string.IsNullOrEmpty(value?.Trim())) return null;
+
+        return Regex.Replace(value, "[^0-9A-Za-z ,]", "");
+    }
+
+    public static string? RemoveAcentuation(string text)
+    {
+        if (text is null || string.IsNullOrEmpty(text?.Trim())) return null;
+
+        return
+            System.Web.HttpUtility.UrlDecode(
+                System.Web.HttpUtility.UrlEncode(
+                    text.Trim(), Encoding.GetEncoding(ENCODING)));
+    }
+}
