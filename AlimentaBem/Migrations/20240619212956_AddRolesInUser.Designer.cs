351cdd3687ae9bf7e87dedb41fdb6e03037277e7
Letícia Desiderio <leticia.desiderio@bnpsolucoes.com.br>
2024-06-22 12:42:23 -0300

    refactor(migrations ): change name of namespace

--- AlimentaBem/Migrations/20240619212956_AddRolesInUser.Designer.cs
+++ AlimentaBem/Migrations/20240619212956_AddRolesInUser.Designer.cs
@@ -5,7 +5,7 @@ using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
-using alimenta.bem.db.context;
+using alimenta_bem.db.context;
 
 #nullable disable
 
--- AlimentaBem/Migrations/AlimentaBemContextModelSnapshot.cs
+++ AlimentaBem/Migrations/AlimentaBemContextModelSnapshot.cs
@@ -4,7 +4,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
-using alimenta.bem.db.context;
+using alimenta_bem.db.context;
 
 #nullable disable
 
