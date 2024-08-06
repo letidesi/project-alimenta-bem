82d977fafc88acaeb390e4de18811c2545d5a2f2
Letícia Desiderio <leticia.desiderio@bnpsolucoes.com.br>
2024-08-05 21:26:09 -0300

    feat(organization): add migrations

---
+++ AlimentaBem/Migrations/20240806000703_Organization.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace alimenta_bem.Migrations
+{
+    /// <inheritdoc />
+    public partial class Organization : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Organizations",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    type = table.Column<int>(type: "int", nullable: true),
+                    description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    createdAt = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false),
+                    updatedAt = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: true),
+                    deletedAt = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Organizations", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Organizations");
+        }
+    }
+}
