786327aa6144db952721d8b944071ba081cb2888
Letícia Desiderio <leticia.desiderio@bnpsolucoes.com.br>
2024-08-05 22:20:38 -0300

    feat(organization-requirement): add migrations

---
+++ AlimentaBem/Migrations/20240806011857_OrganizationRequiment.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace alimenta_bem.Migrations
+{
+    /// <inheritdoc />
+    public partial class OrganizationRequiment : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "OrganizationRequirements",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    organizationId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    quantity = table.Column<int>(type: "int", nullable: false),
+                    type = table.Column<int>(type: "int", nullable: false),
+                    createdAt = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false),
+                    updatedAt = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: true),
+                    deletedAt = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OrganizationRequirements", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_OrganizationRequirements_Organizations_organizationId",
+                        column: x => x.organizationId,
+                        principalTable: "Organizations",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrganizationRequirements_organizationId",
+                table: "OrganizationRequirements",
+                column: "organizationId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "OrganizationRequirements");
+        }
+    }
+}
