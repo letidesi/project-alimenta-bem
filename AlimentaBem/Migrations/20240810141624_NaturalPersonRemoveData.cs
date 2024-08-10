1f0dc4c5a4195a57072ded99761ca1464f4ffbf7
Letícia Desiderio <leticia.desiderio@bnpsolucoes.com.br>
2024-08-10 11:19:07 -0300

    feat(migrations): remove property

---
+++ AlimentaBem/Migrations/20240810141624_NaturalPersonRemoveData.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace alimenta_bem.Migrations
+{
+    /// <inheritdoc />
+    public partial class NaturalPersonRemoveData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "cpf",
+                table: "NaturalPersons");
+
+            migrationBuilder.DropColumn(
+                name: "rg",
+                table: "NaturalPersons");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "cpf",
+                table: "NaturalPersons",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "rg",
+                table: "NaturalPersons",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
