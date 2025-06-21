aeffd5d9873aa440e6f9c5c1e338906024356121
leticia.desiderio <leticia.desiderio@bnpsolucoes.com.br>
2025-06-21 17:32:43 -0300

    feat(migrations): remove fields

--- AlimentaBem/Migrations/20250621182629_NaturalPersonFullName.Designer.cs
+++ AlimentaBem/Migrations/20250621182629_NaturalPersonFullName.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using alimenta_bem.db.context;
 
@@ -11,9 +12,11 @@ using alimenta_bem.db.context;
 namespace alimenta_bem.Migrations
 {
     [DbContext(typeof(AlimentaBemContext))]
-    partial class AlimentaBemContextModelSnapshot : ModelSnapshot
+    [Migration("20250621182629_NaturalPersonFullName")]
+    partial class NaturalPersonFullName
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ AlimentaBem/Migrations/20250621182629_NaturalPersonFullName.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace alimenta_bem.Migrations
+{
+    /// <inheritdoc />
+    public partial class NaturalPersonFullName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "firstName",
+                table: "NaturalPersons");
+
+            migrationBuilder.RenameColumn(
+                name: "lastName",
+                table: "NaturalPersons",
+                newName: "name");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "name",
+                table: "NaturalPersons",
+                newName: "lastName");
+
+            migrationBuilder.AddColumn<string>(
+                name: "firstName",
+                table: "NaturalPersons",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
